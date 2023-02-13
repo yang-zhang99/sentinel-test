6d837421cc96b100786fe2608e589b09589e6c35
yang99 <yang.zhang99@outlook.com>
2023-02-13 17:41:51 +0800

    INIT

---
+++ src/main/java/com/example/sentineltest/SentinelTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.sentineltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentinelTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SentinelTestApplication.class, args);
+    }
+
+}
